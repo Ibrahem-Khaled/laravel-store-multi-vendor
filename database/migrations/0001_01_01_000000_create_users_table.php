60dc28b65d5b137f3653657d75205e6c28647b74
ibrahem khaled <xx.vvv48@gmail.com>
2025-05-10 19:53:19 -0700

    fix bug from uuid generation

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('uuid')->unique();
+            $table->uuid('uuid')->unique();
             $table->string('username')->unique();
             $table->string('name');
             $table->string('email')->unique();
