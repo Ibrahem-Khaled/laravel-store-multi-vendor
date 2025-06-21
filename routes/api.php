0f1772c8911a8678537798fa722ddfde05b0d67e
ibrahem khaled <xx.vvv48@gmail.com>
2025-06-21 03:22:54 +0300

    add verified img and fix bug from update user profile

--- routes/api.php
+++ routes/api.php
@@ -12,33 +12,36 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [authController::class, 'login']);
 Route::post('/register', [authController::class, 'register']);
 
-Route::post('/update-profile', [authController::class, 'update']);
-Route::post('/change-password', [authController::class, 'changePassword']);
-Route::get('me', [authController::class, 'user']);
-Route::get('user/{user}', [authController::class, 'getUser']);
-Route::post('/addExpoPushToken', [authController::class, 'addExpoPushToken']);
-Route::post('/delete-account', [authController::class, 'deleteAccount']);
+Route::group([], function () {
+
+    Route::post('/update-profile', [authController::class, 'update']);
+    Route::post('/change-password', [authController::class, 'changePassword']);
+    Route::get('me', [authController::class, 'user']);
+    Route::get('user/{user}', [authController::class, 'getUser']);
+    Route::post('/addExpoPushToken', [authController::class, 'addExpoPushToken']);
+    Route::post('/delete-account', [authController::class, 'deleteAccount']);
 
 
-Route::get('/categories', [mainApiController::class, 'Categories']);
-Route::get('/sub-categories', [mainApiController::class, 'allSubCategories']);
-Route::get('/categories/{category}/sub-categories', [mainApiController::class, 'SubCategories']);
+    Route::get('/categories', [mainApiController::class, 'Categories']);
+    Route::get('/sub-categories', [mainApiController::class, 'allSubCategories']);
+    Route::get('/categories/{category}/sub-categories', [mainApiController::class, 'SubCategories']);
 
-Route::get('/notifications/{type?}', [mainApiController::class, 'Notifications']);
-Route::get('/notifications/unread-count', [mainApiController::class, 'unreadCountNotifications']);
-Route::post('/notifications/{id}/mark-as-read', [mainApiController::class, 'markNotificationAsRead']);
-Route::delete('/notifications/{id}', [mainApiController::class, 'deleteNotification']);
+    Route::get('/notifications/{type?}', [mainApiController::class, 'Notifications']);
+    Route::get('/notifications/unread-count', [mainApiController::class, 'unreadCountNotifications']);
+    Route::post('/notifications/{id}/mark-as-read', [mainApiController::class, 'markNotificationAsRead']);
+    Route::delete('/notifications/{id}', [mainApiController::class, 'deleteNotification']);
 
-Route::get('/products', [productController::class, 'Products']);
-Route::get('/products/{product}', [productController::class, 'Product']);
-Route::get('/products/featured', [productController::class, 'featuredProducts']);
-Route::get('/products/{product}/similars', [ProductController::class, 'similarsProducts']);
-Route::post('/products/{product}/reviews', [ProductController::class, 'addReview']);
-Route::delete('/products/{product}/reviews', [ProductController::class, 'deleteReview']);
-Route::get('/user/favorites', [ProductController::class, 'userFavorites']);
-Route::post('/products/{product}/favorites', [ProductController::class, 'addToFavorites']);
+    Route::get('/products', [productController::class, 'Products']);
+    Route::get('/products/{product}', [productController::class, 'Product']);
+    Route::get('/featured/products', [productController::class, 'featuredProducts']);
+    Route::get('/products/{product}/similars', [ProductController::class, 'similarsProducts']);
+    Route::post('/products/{product}/reviews', [ProductController::class, 'addReview']);
+    Route::delete('/products/{product}/reviews', [ProductController::class, 'deleteReview']);
+    Route::get('/user/favorites', [ProductController::class, 'userFavorites']);
+    Route::post('/favorites/products/{product}', [ProductController::class, 'addToFavorites']);
 
-Route::get('/reservations', [ReservationController::class, 'index']);
-Route::post('/reservations', [ReservationController::class, 'store']);
-Route::put('/reservations/{id}', [ReservationController::class, 'update']);
-Route::delete('/reservations/{id}', [ReservationController::class, 'destroy']);
+    Route::get('/reservations', [ReservationController::class, 'index']);
+    Route::post('/reservations', [ReservationController::class, 'store']);
+    Route::put('/reservations/{id}', [ReservationController::class, 'update']);
+    Route::delete('/reservations/{id}', [ReservationController::class, 'destroy']);
+});
